2981e693eaa1ae6ac59087dd1ac0456f901189a7
alice <alice@imac-seongyongkim>
2016-08-07 00:39:48 -0700

    added ShowLuckyNum

--- src/com/devops/HelloWorld.java
+++ src/com/devops/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
+		System.out.println("Your lucky Number is " + ShowLuckyNum.getLuckyNum());
 	}
 
 }
