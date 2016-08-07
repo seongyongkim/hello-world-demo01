b2e537d5443fbc012d0f69a21d80b93a10d78f31
alice <alice@imac-seongyongkim>
2016-08-06 19:04:06 -0700

    1st Commit

---
+++ src/com/devops/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.devops;
+
+/**
+ * @author skim
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
